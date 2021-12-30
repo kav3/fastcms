50efd9ad98a2c63e820e53d1fdf6539768d4fb24
Kaveh <o9122615360@gmail.com>
2021-12-30 16:04:15 +0330

    add nginx config

---
+++ admin/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ROOT_DIR=/usr/share/nginx/html
+
+echo "Replacing env constants in JS"
+for file in $ROOT_DIR/js/app.*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
+do
+  echo "Processing $file ...";
+
+  sed -i 's|VUE_APP_API|'${VUE_APP_API}'|g' $file
+  sed -i 's|VUE_APP_VER|'${VUE_APP_VER}'|g' $file
+
+done
+
+echo "Starting Nginx"
+nginx -g 'daemon off;'
